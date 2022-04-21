290a41b5e86fe820a10b117192aeacf59800d59d
his_username <hemantnagar28@yahoo.com>
2022-04-21 07:37:57 +0530

    Final And Static Keyword

--- March_19_3_2022_Onwards/src/wednesday_20_4_2022/DemoStatic.java
+++ March_19_3_2022_Onwards/src/wednesday_20_4_2022/DemoStatic.java
@@ -4,9 +4,15 @@ public class DemoStatic {
 
     static int age=23;
     static int age1;
+    String name="Ramesh";
 
     public static void hello(){
+        //System.out.println(name);
+        System.out.println("This is static method");
 
+    }
+    public static void hello(int age){
+       // System.out.println(int age);
         System.out.println("This is static method");
 
     }
